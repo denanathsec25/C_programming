efa63216420c016c454b8f645daaeb953227841b
denanathsec25 <denanaths.ec25@bitsathy.ac.in>
2026-04-06 22:09:23 +0530

    new files are added

---
+++ Strings/cat_without_strcat.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    char str1[100];
+    fgets(str,sizeof(str),stdin);
+    fgets(str1,sizeof(str1),stdin);
+    printf("%s%s",str,str1);
+
+}
---
+++ Strings/frequency_char.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    fgets(str,sizeof(str),stdin);
+    int hash[1000]={0};
+    strlwr(str);
+    for(int i=0;i<strlen(str);i++){
+        int c = str[i];
+        hash[c]++;
+    }
+    for(int i=97;i<124;i++){
+        if(hash[i]>0){
+            char str1= i;
+            printf("%c %d\n",str1,hash[i]);
+
+        }
+    }
+}
