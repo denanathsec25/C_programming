248f8ae0f03d9ea5b748069212ff9c2b2276294d
denanathsec25 <denanaths.ec25@bitsathy.ac.in>
2026-04-28 07:41:50 +0530

    new files were added

---
+++ Strings/vowels_replace_asteric.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    fgets(str,sizeof(str),stdin);
+    strlwr(str);
+    for(int i=0;i<strlen(str);i++){
+        if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'){
+            printf("*");
+        }
+        else{
+            printf("%c",str[i]);
+        }
+    }
+}
