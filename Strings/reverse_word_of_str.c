7d6151a893a31925c347f2a4571dcd9bb718c738
denanathsec25 <denanaths.ec25@bitsathy.ac.in>
2026-04-29 08:27:07 +0530

    new files were added

---
+++ Strings/reverse_word_of_str.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    int start=0;
+    int end=0;
+
+    fgets(str,sizeof(str),stdin);
+    str[strcspn(str, "\r\n")] = '\0';
+    int len=strlen(str);
+    
+    for(int i=0;i<len;i++){
+
+        if(str[i] == ' ' || str[i] == '\0'){
+            end = i - 1;
+
+            for(int j = end; j >= start; j--){
+                printf("%c", str[j]);
+            }
+
+            if(str[i] == ' '){
+                printf(" ");
+            }
+
+            start = i + 1;
+        }
+    }
+
+    for(int j = len - 1; j >= start; j--){
+        printf("%c", str[j]);
+    }
+    
+    return 0;
+}
