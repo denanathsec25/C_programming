d67117a75d41ecf25687e4482961e5916c1bc9df
denanathsec25 <denanaths.ec25@bitsathy.ac.in>
2026-05-29 18:02:58 +0530

    A file is modified

--- Etalvis/Strings/valid_str.c
+++ Etalvis/Strings/valid_str.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
     char str[51];
     fgets(str,sizeof(str),stdin);
     str[strcspn(str, "\n")] = '\0';
-
+    int num=1;
     for(int i=0;str[i]!='\0';i++){
         if(!(str[i] >= '0' && str[i] <= '9')){
             printf("Invalid");
+            num=0;
             break;
         }
     }
-
+    if(num==1)
     printf("Valid");
 }
