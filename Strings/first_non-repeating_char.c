00dede6052642e8dc0e76f7ec39d668796916206
denanathsec25 <denanaths.ec25@bitsathy.ac.in>
2026-06-03 07:20:00 +0530

    Editted a file

--- Strings/first_non-repeating_char.c
+++ Strings/first_non-repeating_char.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
 #include<string.h>
+#include<ctype.h>
+
 int main(){
     char str[100];
-    scanf ( "%[^\n]s", str ) ; 
+    scanf ( "%[^\n]", str ) ; 
     int hash[1000]={0};
     strlwr(str);
-    for(int i=0;i<strlen(str);i++){
-        int c = str[i];
-        hash[c]++;
-    }
-    int count=0;
-    for(int i = 0; i < strlen(str); i++){
-        if(count==0){
-            if(hash[str[i]] == 1){
-                printf("%c ",str[i]);
-                count++;
 
-            }
-        }
+
+    for(int i=0;i<strlen(str);i++){
+        hash[str[i]]++;
+    }
+
+    for(int i = 0; i < strlen(str); i++){
+         if(hash[str[i]] == 1){
+            printf("%c ",str[i]);
+            break;
+        }       
     }
 }
