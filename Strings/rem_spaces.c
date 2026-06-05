a84e7ec8fa1a049b540c082e19120750b5f52290
denanathsec25 <denanaths.ec25@bitsathy.ac.in>
2026-06-05 16:48:27 +0530

    update a file

--- Strings/rem_spaces.c
+++ Strings/rem_spaces.c
@@ -3,6 +3,7 @@
 int main(){
     char str[100];
     fgets(str,sizeof(str),stdin);
+    str[strcspn(str, "\n")] = '\0';
     int len = strlen(str);
     for(int i=0;i<len;i++){
         int ascii=str[i];
