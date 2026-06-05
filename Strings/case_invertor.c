d778e65a2a707128b9143a4af4186598d0505ec7
denanathsec25 <denanaths.ec25@bitsathy.ac.in>
2026-06-05 16:47:02 +0530

    update a file

--- Strings/case_invertor.c
+++ Strings/case_invertor.c
@@ -2,19 +2,20 @@
 #include<string.h>
 int main(){
      char str[100];
-    fgets(str,sizeof(str),stdin);
-    for(int i=0;i<strlen(str);i++){
-        int ascii =str[i];
-        if(ascii>=65 && ascii <=90){
-            str[i]=ascii+32;
-            printf("%c",str[i]);
-        }
-        else if(ascii>=97 && ascii <=122){
-            str[i]=ascii-32;
-            printf("%c",str[i]);
-        }
-        else{
-            printf("%c",str[i]);
-        }
+        fgets(str,sizeof(str),stdin);
+        str[strcspn(str, "\n")] = '\0';
+        for(int i=0;i<strlen(str);i++){
+            int ascii =str[i];
+            if(ascii>=65 && ascii <=90){
+                str[i]=ascii+32;
+                printf("%c",str[i]);
+            }
+            else if(ascii>=97 && ascii <=122){
+                str[i]=ascii-32;
+                printf("%c",str[i]);
+            }
+            else{
+                printf("%c",str[i]);
+            }
     }
 }
--- Strings/cat_without_strcat.c
+++ Strings/cat_without_strcat.c
@@ -5,6 +5,8 @@ int main(){
     char str1[100];
     fgets(str,sizeof(str),stdin);
     fgets(str1,sizeof(str1),stdin);
-    printf("%s%s",str,str1);
+    str[strcspn(str, "\n")] = '\0';
+    str1[strcspn(str1, "\n")] = '\0';
+    printf("%s %s",str,str1);
 
 }
