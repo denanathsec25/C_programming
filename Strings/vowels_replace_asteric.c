b8f68287cadeb333e1efcea8076df57c3320c093
denanathsec25 <denanaths.ec25@bitsathy.ac.in>
2026-06-05 16:49:35 +0530

    update a file

--- Strings/vowels_replace_asteric.c
+++ Strings/vowels_replace_asteric.c
@@ -3,6 +3,7 @@
 int main(){
     char str[100];
     fgets(str,sizeof(str),stdin);
+    str[strcspn(str, "\n")] = '\0';
     strlwr(str);
     for(int i=0;i<strlen(str);i++){
         if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'){
